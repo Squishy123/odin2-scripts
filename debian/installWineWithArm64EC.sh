b642a7e9e90672e42571f136c734a5f2004b1d6c
Christian Wang <christian.gnaw@gmail.com>
2025-10-19 19:45:26 -0400

    added wine script

---
+++ debian/installWineWithArm64EC.sh
@@ -0,0 +1,88 @@
+#!/bin/bash 
+
+# install dependencies
+sudo apt install -y \
+  debhelper-compat \
+  lld \
+  llvm \
+  clang \
+  gcc-mingw-w64-i686 \
+  gcc-mingw-w64-x86-64 \
+  libz-mingw-w64-dev \
+  lzma \
+  flex \
+  bison \
+  quilt \
+  unzip \
+  gettext \
+  pkgconf \
+  icoutils \
+  sharutils \
+  dctrl-tools \
+  imagemagick \
+  librsvg2-bin \
+  fontforge-nox \
+  khronos-api \
+  unicode-data \
+  unicode-idna \
+  unicode-cldr-core \
+  libgl-dev \
+  libxi-dev \
+  libxt-dev \
+  libxmu-dev \
+  libx11-dev \
+  libxext-dev \
+  libxfixes-dev \
+  libxrandr-dev \
+  libavcodec-dev \
+  libxcursor-dev \
+  libxrender-dev \
+  libxkbfile-dev \
+  libxxf86vm-dev \
+  libxxf86dga-dev \
+  libxinerama-dev \
+  libavformat-dev \
+  libglu1-mesa-dev \
+  libxcomposite-dev \
+  libxkbregistry-dev \
+  libxml-libxml-perl \
+  libssl-dev \
+  libv4l-dev \
+  libsdl2-dev \
+  libkrb5-dev \
+  libudev-dev \
+  libpulse-dev \
+  libldap2-dev \
+  unixodbc-dev \
+  libcups2-dev \
+  libcapi20-dev \
+  libvulkan-dev \
+  libopenal-dev \
+  libdbus-1-dev \
+  freeglut3-dev \
+  libunwind-dev \
+  libpcap0.8-dev \
+  libasound2-dev \
+  libgphoto2-dev \
+  libosmesa6-dev \
+  libncurses-dev \
+  libwayland-dev \
+  libfreetype-dev \
+  libgnutls28-dev \
+  libpcsclite-dev \
+  libusb-1.0-0-dev \
+  libgettextpo-dev \
+  libfontconfig-dev \
+  ocl-icd-opencl-dev \
+  libgstreamer-plugins-base1.0-dev
+
+cd ~ 
+mkdir -p sys && cd sys
+
+git clone https://github.com/bylaws/wine.git
+cd wine 
+
+ ./configure --enable-archs=arm64ec,aarch64,i386 --prefix=/usr --with-mingw=clang --disable-tests
+ make -j$(nproc)
+ sudo --preserve-env=PATH make install -j$(nproc)
+
