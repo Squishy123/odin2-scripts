ab60e75140ea90b5c802ec22b29f072c7edbb7fa
Christian Wang <christian.gnaw@gmail.com>
2025-10-19 22:23:48 -0400

    ok

--- debian/installFex.sh
+++ debian/installFex.sh
@@ -49,4 +49,35 @@ ninja
 sudo ninja install
 sudo ninja binfmt_misc
 
+# configure fex
+# install dependencies
+sudo apt install -y debootstrap patchelf squashfs-tools
+
+# need to add this to bashrc for certain commands necessary in chroot
+if ! grep -q '/sbin:/bin' ~/.bashrc; then
+    echo 'export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:$PATH' >> ~/.bashrc
+fi
+
+source ~/.bashrc
+
+# fetch and extract ubuntu rootfs
+wget "https://rootfs.fex-emu.gg/Ubuntu_24_04/2025-03-04/Ubuntu_24_04.sqsh" -P ~/.fex-emu/RootFS/ 
+mkdir -p ~/.fex-emu/RootFS/Ubuntu_24_04
+unsquashfs -d ~/.fex-emu/RootFS/Ubuntu_24_04 ~/.fex-emu/RootFS/Ubuntu_24_04.sqsh
+
+# execute commands inside chroot to get drivers
+cd ~/.fex-emu/RootFS/Ubuntu_24_04/ 
+
+
+# have to rewrite the chroot command to allow us to pass in commands 
+sed -i '/ChrootArgs\.append(os\.environ\['\''SHELL'\''\])/,+1c\
+        if len(sys.argv) > 2:\
+            ChrootArgs.extend(sys.argv[2:])\
+        else:\
+            ChrootArgs.append(os.environ['\''SHELL'\''])\
+            ChrootArgs.append("-i")' chroot.py
+
+
+./chroot.py chroot /bin/bash -c "dpkg --add-architecture i386 && apt-get update && apt install -y mesa-vulkan-drivers libglx-mesa0:i386 mesa-vulkan-drivers:i386 libgl1-mesa-dri:i386"
+
 
--- debian/installWineWithArm64EC.sh
+++ debian/installWineWithArm64EC.sh
@@ -98,3 +98,22 @@ cd wine
 make -j$(nproc)
 sudo --preserve-env=PATH make install -j$(nproc)
 
+
+# build fex for arm64ec and also wow64
+cd ~/sys/FEX 
+
+mkdir build-arm64ec
+cd build-arm64ec
+cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=../Data/CMake/toolchain_mingw.cmake -DCMAKE_INSTALL_LIBDIR=/usr/lib/wine/aarch64-windows -DENABLE_LTO=False -DMINGW_TRIPLE=arm64ec-w64-mingw32 -DBUILD_TESTING=False -DENABLE_JEMALLOC_GLIBC_ALLOC=False -DCMAKE_INSTALL_PREFIX=/usr ..
+ninja
+sudo ninja install
+
+cd ..
+mkdir build-wow64
+cd build-wow64
+cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=../Data/CMake/toolchain_mingw.cmake -DCMAKE_INSTALL_LIBDIR=/usr/lib/wine/aarch64-windows -DENABLE_LTO=False -DMINGW_TRIPLE=aarch64-w64-mingw32 -DBUILD_TESTING=False -DENABLE_JEMALLOC_GLIBC_ALLOC=False -DCMAKE_INSTALL_PREFIX=/usr ..
+ninja
+sudo ninja install
+cd ..
+
+
