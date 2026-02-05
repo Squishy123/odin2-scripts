c3d49190d827da152fd447daa5b480e5a3988b96
Christian Wang <christian.gnaw@gmail.com>
2026-02-04 21:35:55 -0500

    updated installFex script

--- debian/installFex.sh
+++ debian/installFex.sh
@@ -79,6 +79,8 @@ source ~/.bashrc
 rm chroot.py
 wget https://raw.githubusercontent.com/FEX-Emu/RootFS/refs/heads/main/Scripts/chroot.py 
 
+chmod a+x chroot.py
+
 ./chroot.py chroot /bin/bash -c "dpkg --add-architecture i386 && apt-get update && apt upgrade -y && apt install -y vulkan-tools libgl1-mesa-dri mesa-vulkan-drivers libglx-mesa0:i386 mesa-vulkan-drivers:i386 libgl1-mesa-dri:i386"
 
 
