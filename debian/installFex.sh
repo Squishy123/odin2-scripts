4c4327e625458d67f7cf72b441a264e40de65e14
Christian Wang <christian.gnaw@gmail.com>
2026-02-04 21:37:40 -0500

    ok

--- debian/installFex.sh
+++ debian/installFex.sh
@@ -81,7 +81,7 @@ wget https://raw.githubusercontent.com/FEX-Emu/RootFS/refs/heads/main/Scripts/ch
 
 chmod a+x chroot.py
 
-./chroot.py chroot /bin/bash -c "dpkg --add-architecture i386 && apt-get update && apt upgrade -y && apt install -y vulkan-tools libgl1-mesa-dri mesa-vulkan-drivers libglx-mesa0:i386 mesa-vulkan-drivers:i386 libgl1-mesa-dri:i386"
+sudo ./chroot.py chroot /bin/bash -c "dpkg --add-architecture i386 && apt-get update && apt upgrade -y && apt install -y vulkan-tools libgl1-mesa-dri mesa-vulkan-drivers libglx-mesa0:i386 mesa-vulkan-drivers:i386 libgl1-mesa-dri:i386"
 
 
 # configure fex to use chroot 
