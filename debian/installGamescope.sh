9c0000c65147e8dbb6ce995f632d8f7afcaf06c7
Christian Wang <christian.gnaw@gmail.com>
2026-02-11 00:59:52 -0500

    ok

--- debian/installGamescope.sh
+++ debian/installGamescope.sh
@@ -12,7 +12,7 @@ git submodule update --init
 ~/sys/odin2-scripts/install-meson-deps.sh meson.build 
 
 # dependencies that were missing 
-sudo apt install -y gslang-tools libluajit-5.1-dev 
+sudo apt install -y libluajit-5.1-dev 
 
 meson setup build/
 ninja -C build/
