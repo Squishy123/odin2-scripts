78d410e25be9943968992e00da21368f364dbd09
Christian Wang <christian.gnaw@gmail.com>
2026-02-16 16:56:12 -0500

    ok

--- ubuntu/installLutris.sh
+++ ubuntu/installLutris.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
 
 wget https://github.com/lutris/lutris/releases/download/v0.5.20/lutris_0.5.20_all.deb 
-sudo apt install -y ./lutris_0.5.20_all.deb 
+sudo apt install -y --install-recommends ./lutris_0.5.20_all.deb 
 
