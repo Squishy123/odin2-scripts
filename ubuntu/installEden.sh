80c6d5bdd9d2a67bf6d13bddd54517c6f1cbd1ca
Christian Wang <christian.gnaw@gmail.com>
2026-02-17 13:52:40 -0500

    ok

--- ubuntu/installEden.sh
+++ ubuntu/installEden.sh
@@ -1,16 +1,21 @@
 #!/bin/bash 
 
+mkdir ~/sys/eden 
+
+cd ~/sys/eden
+
 wget https://github.com/eden-emulator/Releases/releases/download/v0.2.0-rc1/Eden-Linux-v0.2.0-rc1-aarch64-gcc-standard.AppImage 
 
-mv Eden-Linux-v0.2.0-rc1-aarch64-gcc-standard.AppImage ~/sys/Eden-Linux-v0.2.0-rc1-aarch64-gcc-standard.AppImage 
-chmod +x ~/sys/Eden-Linux-v0.2.0-rc1-aarch64-gcc-standard.AppImage 
+chmod +x Eden-Linux-v0.2.0-rc1-aarch64-gcc-standard.AppImage 
+
+./Eden-Linux-v0.2.0-rc1-aarch64-gcc-standard.AppImage --appimage-extract
 
 cat > "$HOME/.local/share/applications/eden.desktop" <<EOF
 [Desktop Entry]
 Type=Application
 Name=Eden
 Comment=Launch Eden 
-Exec="$HOME/sys/Eden-Linux-v0.2.0-rc1-aarch64-gcc-standard.AppImage"
+Exec="$HOME/sys/eden/Eden-Linux-v0.2.0-rc1-aarch64-gcc-standard.AppImage"
 Icon=eden
 Terminal=false
 Categories=Game;
