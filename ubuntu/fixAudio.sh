5daa3ae962c098a81b99a86e4c3c086b9eae73b6
Christian Wang <christian.gnaw@gmail.com>
2026-02-18 17:03:39 -0500

    ok

--- ubuntu/fixAudio.sh
+++ ubuntu/fixAudio.sh
@@ -1,18 +1,18 @@
 sudo apt install -y pulseaudio 
 
-systemctl --user stop pipewire.socket 
-systemctl --user stop pipewire
+systemctl --global stop pipewire.socket 
+systemctl --global stop pipewire
 
 sudp systemctl disable pipewire.socket 
 sudo systemctl disable pipewire
 
-systemctl --user disable pipewire.socket
-systemctl --user disable pipewire 
+systemctl --global disable pipewire.socket
+systemctl --global disable pipewire 
 
 sudo systemctl enable pulseaudio.socket
 sudo systemctl enable pulseaudio
 
-systemctl --user enable pulseaudio.socket
-systemctl --user enable pulseaudio
+systemctl --global enable pulseaudio.socket
+systemctl --global enable pulseaudio
 
-systemctl --user restart pulseaudio.socket
+systemctl --global restart pulseaudio.socket
