baf22aea7e58559f2bc1d450985a43d01f00a811
Christian Wang <christian.gnaw@gmail.com>
2026-02-18 16:05:28 -0500

    ok

---
+++ ubuntu/setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash 
+
+# SETUP SCRIPT TO RUN A BUNCH OF STUFF SEQUENTIALLY 
+
+if [[ $EUID -ne 0 ]]; then
+    echo "Please run as root (use sudo)"
+    exit 1
+fi
+
+./fixAudio.sh 
+
+./installLutris.sh 
+
+./installBox64.sh 
+
+./installFex.sh
+
+./installSteam.sh
+
+./installWaydroid.sh
+
+./installEden.sh
