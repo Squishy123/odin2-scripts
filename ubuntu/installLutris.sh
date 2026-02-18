cba33cf48316067e20bf44de135b624333e670a0
Christian Wang <christian.gnaw@gmail.com>
2026-02-18 16:21:40 -0500

    ok

--- ubuntu/installLutris.sh
+++ ubuntu/installLutris.sh
@@ -2,4 +2,5 @@
 
 wget https://github.com/lutris/lutris/releases/download/v0.5.20/lutris_0.5.20_all.deb 
 sudo apt install -y --install-recommends ./lutris_0.5.20_all.deb 
+sudo apt install -y librsvg2-common # not sure why this isn't included previously but we need it or stuff breaks
 
