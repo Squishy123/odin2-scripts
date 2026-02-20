7ab630a4d3073f416ca5fe014f295bdca5c70da9
Christian Wang <christian.gnaw@gmail.com>
2026-02-19 21:35:02 -0500

    ok

---
+++ ubuntu/installArdour.sh
@@ -0,0 +1,75 @@
+#!/bin/bash 
+
+sudo apt install -y \
+  aubio-tools libaubio-dev \
+  autoconf \
+  automake \
+  bison \
+  libboost-all-dev \
+  libcairo2-dev \
+  libcairomm-1.0-dev \
+  cmake \
+  libcppunit-dev \
+  curl libcurl4-openssl-dev \
+  libexpat1-dev \
+  libfftw3-dev \
+  libflac-dev \
+  flex \
+  libfontconfig1-dev \
+  libfreetype6-dev \
+  libfribidi-dev \
+  gettext \
+  libglib2.0-dev \
+  libglibmm-2.4-dev \
+  gnome-common \
+  gnome-doc-utils \
+  libgirepository1.0-dev \
+  libharfbuzz-dev \
+  intltool \
+  itstool \
+  libjpeg-dev \
+  libarchive-dev \
+  libffi-dev \
+  libc6-dev \
+  liblo-dev \
+  libogg-dev \
+  libpng-dev \
+  libsamplerate0-dev \
+  libsigc++-2.0-dev \
+  libsndfile1-dev \
+  libtool \
+  libusb-1.0-0-dev \
+  libvorbis-dev \
+  libwebsockets-dev \
+  libxml2-dev \
+  libxslt1-dev \
+  liblilv-dev \
+  liblrdf-dev \
+  liblv2-dev \
+  m4 \
+  make \
+  libnss3-dev \
+  libopus-dev \
+  libpango1.0-dev \
+  libpangomm-1.4-dev \
+  libpcre3-dev \
+  libpixman-1-dev \
+  pkg-config \
+  libraptor2-dev \
+  librasqal3-dev \
+  python3-rdflib \
+  libreadline-dev \
+  librdf0-dev \
+  librubberband-dev \
+  libserd-dev \
+  libsord-dev \
+  libsratom-dev \
+  libtag1-dev \
+  tar \
+  libncurses-dev \
+  libtiff-dev \
+  util-linux \
+  uuid-dev \
+  libvamp-sdk2 libvamp-hostsdk3 \
+  liblzma-dev \
+  zlib1g-dev
